153a9254c346a714e9669680df867268700fe6bd
Michael Cheung <mtc5525@students.cs.ubc.ca>
2023-04-07 20:51:51 -0700

    Fix instantiation bug and add number_codes

--- main.pl
+++ main.pl
@@ -14,7 +14,7 @@ start(Ans) :-
     split_string(Input, " -", " ,?.!-", Ln), % ignore punctuation
     find_pokemon(Ln, Ans).
 start(Ans) :-
-    write("Sorry, no more matching Pokemon were found!\n"),
+    write("Sorry, no known matching Pokemon were found!\n"),
     start(Ans).
 
 % find_pokemon(Q,A) gives matching Pokemon A to question Q
@@ -25,24 +25,24 @@ test_individual([]).
 test_individual([H|T]) :- call(H), test_individual(T).
 
 % get_constraints_from_question(Q,A,C) is true if C is the constraints on A to infer question Q
-parse_question(Q,A,C) :-
-    get_question(Q,End,A,C,[]),
-    member(End,[[],["?"],["."]]).
+parse_question(Q, A, C) :-
+    get_question(Q, End, A, C, []),
+    member(End, [[], ["?"], ["."]]).
 
 % get_question(Question,QR,Ind) is true if Query provides an answer about Ind to Question
 % get_question(["Is" | L0],L2,Ind,C0,C2) :-
 %     noun_phrase(L0,L1,Ind,C0,C1),
 %     mp(L1,L2,Ind,C1,C2).
-get_question(["What","is" | L0], L1, Ind,C0,C1) :-
-    aphrase(L0,L1,Ind,C0,C1).
-get_question(["What" | L0],L2,Ind,C0,C2) :-
-    noun_phrase(L0,L1,Ind,C0,C1),
-    mp(L1,L2,Ind,C1,C2).
-get_question(["What" | L0],L1,Ind,C0,C1) :-
-    mp(L0,L1,Ind,C0,C1).
+get_question(["What","is" | L0], L1, Ind, C0, C1) :-
+    aphrase(L0, L1, Ind, C0, C1).
+get_question(["What" | L0], L2, Ind, C0, C2) :-
+    noun_phrase(L0, L1, Ind, C0, C1),
+    mp(L1, L2, Ind, C1, C2).
+get_question(["What" | L0], L1, Ind, C0, C1) :-
+    mp(L0, L1, Ind, C0, C1).
 
-aphrase(L0, L1, E, C0, C1) :- noun_phrase(L0, L1, E,C0,C1).
-aphrase(L0, L1, E, C0, C1) :- mp(L0, L1, E,C0,C1).
+aphrase(L0, L1, E, C0, C1) :- noun_phrase(L0, L1, E, C0, C1).
+aphrase(L0, L1, E, C0, C1) :- mp(L0, L1, E, C0, C1).
 
 noun_phrase(L0,L4,Ind,C0,C4) :-
     det(L0,L1,Ind,C3,C4),
@@ -60,9 +60,9 @@ adjectives(L0,L2,Ind,C0,C2) :-
     adjectives(L1,L2,Ind,C1,C2).
 adjectives(L,L,_,C,C).
 
-adj(["heavy" | L],L,Ind, [heavy(Ind)|C],C).
-adj(["light" | L], L, Ind, [light(Ind)|C], C).
-adj([Typename,"type" | L],L,Ind, [facts:typename(Type, Typename), facts:type(Ind, Type) |C],C).
+adj(["heavy" | L], L, Ind, [heavy(Ind) | C], C).
+adj(["light" | L], L, Ind, [light(Ind) | C], C).
+adj([Typename,"type" | L], L, Ind, [facts:typename(Type, Typename), facts:type(Ind, Type) | C], C).
 
 % heavy(P) is true if the Pokemon P is heavier than 100 kg
 heavy(Ind) :-
@@ -73,25 +73,26 @@ light(Ind) :-
     weightkg(Ind, W),
     W < 20.
 
-noun(["pokemon" | L],L,Ind, [facts:pokemon(Ind)|C],C).
-noun(["Pokemon" | L],L,Ind, [facts:pokemon(Ind)|C],C).
-noun([N | L], L, Ind, C,C) :- name(Ind, N). % Parse fails if there is no entity for name
+noun(["pokemon" | L], L, Ind, [facts:pokemon(Ind)|C], C).
+noun(["Pokemon" | L], L, Ind, [facts:pokemon(Ind)|C], C).
+noun([N | L], L, Ind, C, C) :- name(Ind, N). % Parse fails if there is no entity for name
 
-omp(L0,L1,E,C0,C1) :-
-    mp(L0,L1,E,C0,C1).
-omp(L,L,_,C,C).
+omp(L0, L1, E, C0, C1) :-
+    mp(L0, L1, E, C0, C1).
+omp(L, L, _, C, C).
 
 
-mp(L0,L2,Subject,C0,C2) :-
-    reln_single(L0,L1,Subject,C0,C1).
-mp(["that"|L0],L2,Subject,C0,C2) :-
-    reln_single(L0,L1,Subject,C0,C1).
-mp(["with", "a"|L0],L2,Subject,C0,C2) :-
-    reln_single(L0,L1,Subject,C0,C1).
-mp(["with", "an"|L0],L2,Subject,C0,C2) :-
-    reln_single(L0,L1,Subject,C0,C1).
-mp(["with"|L0],L2,Subject,C0,C2) :-
-    reln_single(L0,L1,Subject,C0,C1).
+mp(L0, L1, Subject, C0, C1) :-
+    reln_single(L0, L1, Subject, C0, C1).
+mp(["that" | L0], L1, Subject, C0, C1) :-
+    reln_single(L0, L1, Subject, C0, C1).
+mp(["with", "a" | L0], L1, Subject, C0, C1) :-
+    reln_single(L0, L1, Subject, C0, C1).
+mp(["with", "an" | L0], L1, Subject, C0, C1) :-
+    reln_single(L0, L1, Subject, C0, C1).
+mp(["with" | L0], L1, Subject, C0, C1) :-
+    reln_single(L0, L1, Subject, C0, C1).
+
 
 % mp(L0,L2,Subject,C0,C2) :-
 %     reln(L0,L1,Subject,Object,C0,C1),
@@ -103,23 +104,23 @@ mp(["with"|L0],L2,Subject,C0,C2) :-
 %     reln(L0,L1,Subject,Object,C0,C1),
 %     aphrase(L1,L2,Object,C1,C2).
 
-reln_single(["weight", "greater", "than", Number, "lb" | L],L,Sub,[facts:weightlb(Sub,Val), number_codes(Num, Number), Val > Num | C ], C).
-reln_single(["weight", "larger", "than", Number, "lb" | L],L,Sub,[facts:weightlb(Sub,Val), Val > Number | C ], C).
-reln_single(["weight", "bigger", "than" , Number , "lb" | L],L,Sub,[facts:weightlb(Sub,Val), Val > Number | C ], C).
-reln_single(["weight", "less", "than" , Number , "lb" | L],L,Sub,[facts:weightlb(Sub,Val), Val < Number | C ], C).
-reln_single(["weight", "smaller", "than", Number, "lb" | L],L,Sub,[facts:weightlb(Sub,Val), Val < Number | C ], C).
+reln_single(["weight", "greater", "than", Number, "lb" | L], L, Sub, [facts:weightlb(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["weight", "larger", "than", Number, "lb" | L], L, Sub, [facts:weightlb(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["weight", "bigger", "than" , Number , "lb" | L], L, Sub, [facts:weightlb(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["weight", "less", "than" , Number , "lb" | L], L, Sub, [facts:weightlb(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
+reln_single(["weight", "smaller", "than", Number, "lb" | L], L, Sub, [facts:weightlb(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
 
-reln_single(["weight", "greater", "than", Number | L],L,Sub,[facts:weightkg(Sub,Val), number_codes(Num, Number), Val > Num | C], C).
-reln_single(["weight", "larger", "than", Number | L],L,Sub,[facts:weightkg(Sub,Val), Val > Number | C ], C).
-reln_single(["weight", "bigger", "than", Number | L],L,Sub,[facts:weightkg(Sub,Val), Val > Number | C ], C).
-reln_single(["weight", "less", "than", Number | L],L,Sub,[facts:weightkg(Sub,Val), Val < Number | C ], C).
-reln_single(["weight", "smaller", "than", Number | L],L,Sub,[facts:weightkg(Sub,Val), Val < Number | C ], C).
+reln_single(["weight", "greater", "than", Number | L], L, Sub, [facts:weightkg(Sub,Val), number_codes(Num, Number), Val > Num | C], C).
+reln_single(["weight", "larger", "than", Number | L], L, Sub, [facts:weightkg(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["weight", "bigger", "than", Number | L], L, Sub, [facts:weightkg(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["weight", "less", "than", Number | L], L, Sub, [facts:weightkg(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
+reln_single(["weight", "smaller", "than", Number | L], L, Sub, [facts:weightkg(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
 
-reln_single(["height", "greater", "than",  Number | L],L,Sub,[facts:height_m(Sub,Val), Val > Number | C ], C).
-reln_single(["height", "larger", "than" , Number | L],L,Sub,[facts:height_m(Sub,Val), Val > Number | C ], C).
-reln_single(["height", "bigger", "than" , Number | L],L,Sub,[facts:height_m(Sub,Val), Val > Number | C ], C).
-reln_single(["height", "less", "than" , Number | L],L,Sub,[facts:height_m(Sub,Val), Val < Number | C ], C).
-reln_single(["height", "smaller", "than" , Number | L],L,Sub,[facts:height_m(Sub,Val), Val < Number | C ], C).
+reln_single(["height", "greater", "than",  Number | L], L, Sub, [facts:height_m(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["height", "larger", "than" , Number | L], L, Sub, [facts:height_m(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["height", "bigger", "than" , Number | L], L, Sub, [facts:height_m(Sub, Val), number_codes(Num, Number), Val > Num | C ], C).
+reln_single(["height", "less", "than" , Number | L], L, Sub, [facts:height_m(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
+reln_single(["height", "smaller", "than" , Number | L], L, Sub, [facts:height_m(Sub, Val), number_codes(Num, Number), Val < Num | C ], C).
 
 
 % reln(["bordering" | L],L,Sub,Obj,[borders(Sub,Obj)|C],C).
